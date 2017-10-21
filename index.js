3a6b03ecbc930b7a7b1ae0f254cd8e9da9401039
Karan Arora <karan.arora@comprotechnologies.com>
2017-10-21 16:44:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile('D:/MyProjects/socketioexampleapp/index.html');
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat msg', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat msg', msg);
+    });
+    console.log('a user connected');
+});
+
+http.listen(5001, function(){
+    console.log("started");
+});
